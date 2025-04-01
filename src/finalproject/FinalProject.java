c110b829dd4a5dc7bee4c2e39a792fe2b7482edd
J3ruel <jerueljemsvelayo@gmail.com>
2025-04-01 23:54:36 +0800

    added Log in and Sign in

---
+++ src/finalproject/FinalProject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package finalproject;
+
+/**
+ *
+ * @author Jeruel
+ */
+public class FinalProject {
+
+    public static void main(String[] args) {
+        
+        start st = new start();
+        st.setVisible(true);
+       
+    }
+    
+}
